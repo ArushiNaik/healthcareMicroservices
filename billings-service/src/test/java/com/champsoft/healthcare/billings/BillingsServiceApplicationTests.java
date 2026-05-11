7f434249056d5e08ab9a34fcc645efb685dec19d
marwamesk <maroumes12@hotmail.com>
2026-05-10 21:18:12 -0400

    Billing module almost done

--- billings-service/src/test/java/com/champsoft/healthcare/billings/BillingsServiceApplicationTests.java
+++ billings-service/src/test/java/com/champsoft/healthcare/billings/BillingsServiceApplicationTests.java
@@ -3,11 +3,16 @@ package com.champsoft.healthcare.billings;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("testing")
 class BillingsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
+
